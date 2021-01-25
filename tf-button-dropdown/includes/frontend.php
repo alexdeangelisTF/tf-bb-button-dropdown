73a20be49a569b269de4f2e629a11eef96df8f14
Alex De Angelis <alex@activeimage.co.uk>
2021-01-25 15:58:02 +0000

    removing old code

--- tf-button-dropdown/includes/frontend.php
+++ tf-button-dropdown/includes/frontend.php
@@ -1,31 +1,3 @@
-<?php /* ?>
-<div class="dropdown dropdown-format d-inline-block">
-	<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-		Select Format
-	</button>
-
-	<div class="dropdown-menu">
-		<?php
-		foreach($book_buy_links as $book_format) {
-			// Within the loop, store the Format, ISBN, Cover Image, Format specific synopsis & Buy Links repeater
-			$book_format_ID = false;
-			$book_format_ID = $book_format['book_format'];
-			$book_format_name = get_the_title($book_format_ID);
-			$book_format_object = get_post($book_format_ID);
-			$book_format_postname = $book_format_object->post_name;
-			?>
-			<a class="dropdown-item dropdown-item-format" id="<?php echo $book_format_postname; ?>" href="#">
-				<?php echo $book_format_name; ?>
-			</a>
-
-		<?php
-		}
-
-		?>
-	</div>
-</div>
-<?php */ ?>
-
 <div class="tf-dropdown tf-dropdown-<?php echo $id; ?>">
 	<button type="button" class="tf-button" data-toggle="dropdown">
 		Select Format
