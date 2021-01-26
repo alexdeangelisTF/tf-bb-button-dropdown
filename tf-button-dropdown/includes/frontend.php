99e8fc6a96df3422cbad802cc509910a150b6637
Alex De Angelis <alex@activeimage.co.uk>
2021-01-26 11:34:43 +0000

    variable updates

--- tf-button-dropdown/includes/frontend.php
+++ tf-button-dropdown/includes/frontend.php
@@ -1,29 +1,42 @@
 <?php
 // Get the value of the repeater field in the module
 // This should be the field name of the repeater for button one
-$tf_button_1_repeater_value = $settings->tf_button_dropdown_button_1_repeater_field;
+$button_1_repeater_value = $settings->tf_button_dropdown_button_1_repeater_field;
 // Check if this field name matches in the db
-$tf_button_1_repeater_field = get_field($tf_button_1_repeater_value);
+$button_1_repeater_field = get_field($button_1_repeater_value);
 // If a field name with a value has been found, show the button
-if ($tf_button_1_repeater_field) {
+if ($button_1_repeater_field) {
 	
 	// Get label field value
-	$label_value = $settings->tf_button_dropdown_button_1_label_field;
-	// Get link field value
-	$link_value = $settings->tf_button_dropdown_button_1_link_field;
+	$button_1_label_value = $settings->tf_button_dropdown_button_1_label_field;
 	
-	// There must be a lable & link value in order to show the button
-	if ($label_value && $link_value) {
+	$button_1_text = $settings->tf_button_dropdown_button_1_text;
+	if (!$button_1_text) {
+		$button_1_text = 'Button text';
+	}
+	
+	// There must be a label in order to show the button
+	if ($button_1_label_value) {
+		
+		$button_1_link_type = $settings->tf_button_dropdown_button_1_link_type;
+		$button_1_link_value = $settings->tf_button_dropdown_button_1_link_field;
 		
 		echo '<div class="tf-dropdown tf-dropdown-' . $id . '">';
 		echo '<button type="button" class="tf-button" data-toggle="dropdown">';
 		// This text should change to a module field value
-		echo 'Select Format';
+		echo $button_1_text;
 		echo '</button>';
 		echo '<div class="tf-dropdown-menu">';
-		foreach($tf_button_1_repeater_field as $repeater_1) {
-			echo '<a class="tf-dropdown-item" id="postname-1" href="' . $repeater_1[$link_value] . '">';
-			echo $repeater_1[$label_value];
+		foreach($button_1_repeater_field as $repeater_1) {
+			
+			if ($button_1_link_type == 'link_type_url_field') {
+				$link = $repeater_1[$button_1_link_value];
+			} elseif ($button_1_link_type == 'link_type_repeater_field') {
+				$link = '#';
+			} else {}
+			
+			echo '<a class="tf-dropdown-item" id="postname-1" href="' . $link . '">';
+			echo $repeater_1[$button_1_label_value];
 			echo '</a>';
 		}
 		echo '</div>';
