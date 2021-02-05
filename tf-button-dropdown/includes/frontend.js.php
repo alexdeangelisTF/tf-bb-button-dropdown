3549df9d80778a65a638daeff6f5ebc435ed0611
Alex De Angelis <alex@activeimage.co.uk>
2021-02-05 09:21:30 +0000

    show drowpdown 2 on click

--- tf-button-dropdown/includes/frontend.js.php
+++ tf-button-dropdown/includes/frontend.js.php
@@ -5,8 +5,14 @@
 ?>
 
 (function($) {
+	// Button 1 Dropdown on click
 	$('.tf-dropdown-<?php echo $id; ?> #button-1 button').on('click',function(e){
 		e.preventDefault();
 		$('.tf-dropdown-<?php echo $id; ?> #button-1 .tf-dropdown-menu').toggle();
+	});
+	// Button 2 Dropdown on click
+	$('.tf-dropdown-<?php echo $id; ?> #button-2 button').on('click',function(e){
+		e.preventDefault();
+		$('.tf-dropdown-<?php echo $id; ?> #button-2 .tf-dropdown-menu').toggle();
 	});
 })( jQuery );
