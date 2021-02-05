97d0ef4bc348668ece864099c0d7892a96d099d8
Alex De Angelis <alex@activeimage.co.uk>
2021-02-05 14:01:41 +0000

    custom hide/show classes

--- tf-button-dropdown/includes/frontend.css.php
+++ tf-button-dropdown/includes/frontend.css.php
@@ -4,13 +4,18 @@
 
 ?>
 
+.tf-dropdown-<?php echo $id; ?> .hide {
+	display:none;
+}
+.tf-dropdown-<?php echo $id; ?> .show {
+	display:block;
+}
 .tf-dropdown-<?php echo $id; ?> .tf-dropdown-menu {
 	position: absolute;
 	background-color: #f1f1f1;
 	min-width: 160px;
 	overflow: auto;
 	box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-	display:none;
 }
 .tf-dropdown-<?php echo $id; ?> .tf-dropdown-menu a {
 	color: black;
