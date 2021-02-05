ae726ca9ea54d0e2f0b7515253cfe62775840940
Alex De Angelis <alex@activeimage.co.uk>
2021-02-05 09:21:54 +0000

    hide dropdowns before click

--- tf-button-dropdown/includes/frontend.css.php
+++ tf-button-dropdown/includes/frontend.css.php
@@ -10,8 +10,6 @@
 	min-width: 160px;
 	overflow: auto;
 	box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-}
-.tf-dropdown-<?php echo $id; ?> .tf-dropdown-menu-1 {
 	display:none;
 }
 .tf-dropdown-<?php echo $id; ?> .tf-dropdown-menu a {
@@ -23,6 +21,8 @@
 .tf-dropdown-<?php echo $id; ?> .tf-dropdown-menu a:hover {
 	background-color:#ddd;
 }
+/*Hide the list items in the dropdown initially
 .tf-dropdown-<?php echo $id; ?> .tf-dropdown-menu-2 a {
 	display:none;
 }
+*/
